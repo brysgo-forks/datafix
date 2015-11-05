8f709d06a94d80723f27c7adbe9cc4c277b46c4c
Bryan Goldstein <brysgo@gmail.com>
2015-11-04 19:19:36 -0500

    Switch back to NOW() and fix syntax error

--- lib/datafix.rb
+++ lib/datafix.rb
@@ -24,7 +24,7 @@ class Datafix
       execute(<<-SQL)
       INSERT INTO datafix_log
       (direction, script, timestamp)
-      VALUES ('#{direction}', '#{name.camelize}', TIMESTAMP))
+      VALUES ('#{direction}', '#{name.camelize}', NOW())
       SQL
     end
 
